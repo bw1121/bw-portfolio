a1b60ff8fc23ff4a0b21681f1b39c555340bf302
Bomyee Woo <bomyeew@gmail.com>
2020-12-05 09:46:41 -0500

    Add header

--- functions.php
+++ functions.php
@@ -5,22 +5,81 @@
 Add stylesheets and javascripts files
 
 ==========================================================*/ 
-
 function custom_theme_scripts() {
-    //Bootstrap CSS
-    wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . 'css/bootstrap.min.css' );
+    //Boostrap CSS
+    wp_enqueue_style('boostrap', get_stylesheet_directory_uri() . '/css/bootstrap.min.css');
 
     //Main CSS
-    wp_enqueue_style( 'main-styles', get_stylesheet_uri() );
+    wp_enqueue_style('main-styles', get_stylesheet_uri());
+
 
     //jQuery
-    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script('jquery');
 
-    //Javascript Files
-    wp_enqueue_script( 'bootstrap-js', get_stylesheet_directory_uri() . 'js/bootstrap.min.js' );
-    wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . 'js/scripts.js' );
+    //JavaScript
+    wp_enqueue_script('bootstrap-js', get_stylesheet_directory_uri() . '/js/bootstrap.min.js');
+    
 }
 
 add_action('wp_enqueue_scripts', 'custom_theme_scripts');
+
+
+
+/*========================================================
+
+Add featured images
+
+==========================================================*/ 
+add_theme_support('post-thumbnails');
+
+
+
+/*========================================================
+
+Custom Header Image (logo)
+
+==========================================================*/ 
+$custom_image_header = array(
+    'width'     => 250, 
+    'height'    => 70,
+    'uploads'   => true
+);
+
+add_theme_support( 'custom-header', $custom_image_header);
+
+
+
+/*========================================================
+
+Add Menus
+
+==========================================================*/ 
+
+function register_my_menus() {
+    register_nav_menus(array(
+        'main-menu'         => __('Main Menu')
+    ));
+}
+
+add_action('init', 'register_my_menus');
+
+
+
+/*========================================================
+
+Register Custom Navigation Walker
+
+==========================================================*/
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
+
+function prefix_modify_nav_menu_args( $args ) {
+    return array_merge( $args, array(
+        'walker' => new WP_Bootstrap_Navwalker(),
+    ) );
+}
+add_filter( 'wp_nav_menu_args', 'prefix_modify_nav_menu_args' );
 
 ?>
--- header.php
+++ header.php
@@ -4,10 +4,53 @@
         <meta charset="<?php bloginfo('charset'); ?>"> 
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- GOOGLE FONTS -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Raleway:wght@700&display=swap" rel="stylesheet">
+
         <title><?php bloginfo('name'); ?></title>
 
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
+        <!-- HEADER -->
+        <header>
+                <!-- NAVIGATION -->
+                <div class="container sticky-top" id="nav">
+                    <nav class="navbar navbar-expand-md navbar-light" role="navigation">
+                        <!-- LOGO -->
+                        <div class="navbar-brand">
+                            <?php 
+                                // Display logo if it's set; if not, display site title
+                                if(get_header_image() == ''){ ?>
+                                    <h1><a href="<?php echo get_home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+                                <?php } else { ?>
+                                    <a href="<?php get_home_url(); ?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Logo"></a>
+                            <?php } ?>
+                        </div>
+                        
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <!-- MENU ITEMS --> 
+                        <div class="menu-items">
+                            <?php
+                            wp_nav_menu( array(
+                                'theme_location'    => 'main-menu',
+                                'depth'             => 2,
+                                'container'         => 'div',
+                                'container_class'   => 'collapse navbar-collapse',
+                                'container_id'      => 'bs-example-navbar-collapse-1',
+                                'menu_class'        => 'nav navbar-nav',
+                                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                                'walker'            => new WP_Bootstrap_Navwalker(),
+                            ) );
+                            ?>
+                        </div>
+                    </nav>
+                </div><!-- #nav -->
+            </header>
 
 
