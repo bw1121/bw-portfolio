79c0832b1f4a397640da7115af25bb38bff5f981
Bomyee Woo <bomyeew@gmail.com>
2020-12-05 11:10:01 -0500

    Add home hero

--- front-page.php
+++ front-page.php
@@ -1,3 +1,23 @@
 <?php get_header(); ?>
 
+<div class="home-hero">
+    <div class="container hero-content">
+        <div class="row">
+            <div class="hero-img col-md-6 col-lg-5">
+                <?php the_post_thumbnail('full'); ?>
+            </div>
+            <div class="hero-copy col-md-6 col-lg-7">
+                <?php 
+                    if (have_posts()) {
+                        while(have_posts()) {
+                            the_post(); 
+                            the_content();
+                        }
+                    }
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php get_footer(); ?>
